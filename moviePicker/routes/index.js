a06e60beb1a5f25ec80b0e66ddc8a000d32284f5
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-10-31 16:49:14 +0000

    splitting into states

--- moviePicker/routes/index.js
+++ moviePicker/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var movieNights = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log("sending main");
+    res.sendFile('movies.html', { root: 'public' });
+});
+
+router.get('/join', function(req, res, next) {
+    var movieNight = req.query["q"];
+    if (movieNights.indexOf(movieNight) > 0) {
+        // do something
+    }
+})
+
+router.post('/create', function(req, res) {
+    console.log("In Create Session");
+    console.log(req.body);
+    movieNights.push(req.body.session);
+    console.log("Movie Nights:" + movieNights.toString());
+    res.send('{"success" : "Updated Successfully", "status" : 200}');
 });
 
 module.exports = router;
