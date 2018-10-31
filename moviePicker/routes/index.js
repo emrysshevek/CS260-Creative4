1268aca754722e48bde0998196a7950feb0541b6
joshclement0 <joshclement0@gmail.com>
2018-10-31 15:50:18 +0000

    backend

--- moviePicker/routes/index.js
+++ moviePicker/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', { root: 'public' });
 });
-
+var keys=[];
+var movieDisplay=[];
+var movieQueue=[];
+router.get('/checkKey', function(req,res,next){
+    var jsonResult;
+    var userkey=req.query['k'];
+    for(key in keys){
+        if(key===userkey){
+            jsonResult=movieDisplay[key];
+            res.status(200).json(jsonResult);
+        }
+    }
+    
+})
 module.exports = router;
