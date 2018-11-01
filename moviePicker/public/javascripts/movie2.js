ead8319539554e99ee664aa225e4784ea10de4c7
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-11-01 17:23:50 +0000

    did some stuff

--- moviePicker/public/javascripts/movie2.js
+++ moviePicker/public/javascripts/movie2.js
@@ -5,9 +5,9 @@ var pagenum = 1;
 var infotext = "IT'S MOVIE NIGHT, and you haven't yet decided on a movie, instead of randomly listing off movie names that you have already watch, let us help. Press start and we will give you a list of all movies based on popularity from there, if you decide you like it, you can press the thumbs up if you want to move it's position closer to the top, the 'x' to have it removed from the list, or the movie picture to view a description and other facts. At any time, you can press the start button to start the process over again. Enjoy!"
 var app = angular.module('app', ['ui.router']);
 
-// app.config(function($sceDelegateProvider) {
-//     $sceDelegateProvider.resourceUrlWhitelist(['**']);
-// });
+app.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist(['**']);
+});
 
 app.config([
     '$stateProvider',
@@ -42,44 +42,7 @@ app.factory("movieFactory", [function() {
 app.controller('MainCtrl',
     function($scope, $http, movieFactory) {
         console.log("Main Controller");
-        $scope.moviequeue = movieFactory.movies;
-        $scope.movieToqueue = movieFactory.moviestoqueue;
-        $scope.moviepopup = movieFactory.moviepopup;
         $scope.info = infotext;
-
-        $scope.onup = function() {
-            $scope.info = ""
-            $scope.moviequeue = [];
-            $scope.movieToqueue = [];
-            var url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7678944848f7b822b6b11c2978c94dea";
-            $http.get(url).then(function(response) {
-                for (x in response["data"]["results"]) {
-                    var imgposter = imageurl + response["data"]["results"][x]["poster_path"]
-                    var imgbkgrnd = imageurl + response["data"]["results"][x]["backdrop_path"]
-                    if (x < 12) {
-                        $scope.moviequeue.push({
-                            title: response["data"]["results"][x]["title"],
-                            about: response["data"]["results"][x]["overview"],
-                            upvotes: 0,
-                            nameimage: imgposter,
-                            backgrdimage: imgbkgrnd,
-                            vote: response["data"]["results"][x]["vote_average"]
-                        });
-                    }
-                    else {
-                        $scope.movieToqueue.push({
-                            title: response["data"]["results"][x]["title"],
-                            about: response["data"]["results"][x]["overview"],
-                            upvotes: 0,
-                            nameimage: imgposter,
-                            backgrdimage: imgbkgrnd,
-                            vote: response["data"]["results"][x]["vote_average"]
-                        });
-                    }
-                }
-            });
-            $("#info").html = "";
-        };
 
         $scope.createSession = function() {
             $scope.session = "Mason's Movie Night";
@@ -110,6 +73,43 @@ app.controller('MovieCtrl',
         $scope.moviequeue = movieFactory.movies;
         $scope.movieToqueue = movieFactory.moviestoqueue;
         $scope.moviepopup = movieFactory.moviepopup;
+        console.log($scope.moviequeue);
+
+        $scope.init = function() {
+            console.log("init");
+            $scope.info = ""
+            $scope.moviequeue = [];
+            $scope.movieToqueue = [];
+            var url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7678944848f7b822b6b11c2978c94dea";
+            $http.get(url).then(function(response) {
+                for (x in response["data"]["results"]) {
+                    var imgposter = imageurl + response["data"]["results"][x]["poster_path"]
+                    var imgbkgrnd = imageurl + response["data"]["results"][x]["backdrop_path"]
+                    if (x < 12) {
+                        $scope.moviequeue.push({
+                            title: response["data"]["results"][x]["title"],
+                            about: response["data"]["results"][x]["overview"],
+                            upvotes: 0,
+                            nameimage: imgposter,
+                            backgrdimage: imgbkgrnd,
+                            vote: response["data"]["results"][x]["vote_average"]
+                        });
+                    }
+                    else {
+                        $scope.movieToqueue.push({
+                            title: response["data"]["results"][x]["title"],
+                            about: response["data"]["results"][x]["overview"],
+                            upvotes: 0,
+                            nameimage: imgposter,
+                            backgrdimage: imgbkgrnd,
+                            vote: response["data"]["results"][x]["vote_average"]
+                        });
+                    }
+                }
+            });
+            console.log($scope.moviequeue);
+            $("#info").html = "";
+        };
 
         $scope.popup = function(movie) {
             console.log("in pop up")
